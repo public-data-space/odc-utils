0c7634d3f84d9e1545b4759a53f39f88222bbcee
Vincent Bohlen <vincent.bohlen@fokus.fraunhofer.de>
2019-08-07 09:57:11 +0200

    Manager AssetID added to DataAssetCreateMessage for easier handling in adapters

--- src/main/java/de/fraunhofer/fokus/ids/messages/DataAssetCreateMessage.java
+++ src/main/java/de/fraunhofer/fokus/ids/messages/DataAssetCreateMessage.java
@@ -5,8 +5,17 @@ import de.fraunhofer.fokus.ids.persistence.entities.Job;
 
 public class DataAssetCreateMessage {
 
-    Job job;
-    DataSource dataSource;
+    private Job job;
+    private DataSource dataSource;
+    private Long dataAssetId;
+
+    public Long getDataAssetId() {
+        return dataAssetId;
+    }
+
+    public void setDataAssetId(Long dataAssetId) {
+        this.dataAssetId = dataAssetId;
+    }
 
     public Job getJob() {
         return job;
